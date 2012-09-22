c1a69f3632e3851104d44656f33c8c73541a809f
joris <jorisd@gmail.com>
2012-09-23 00:08:12 +0200

    gros debug, me manquait des notions de jquery/ajax etc

--- webservice/highcharts/Joris-Webservice/bin/app.pl
+++ webservice/highcharts/Joris-Webservice/bin/app.pl
@@ -8,9 +8,10 @@ use 5.010;
 use Dancer;
 use Joris::Webservice;
 use DBI;
+use DateTime;
 
 sub connect_db {
-  my $dbh = DBI->connect("dbi:SQLite:dbname=db.sqlite3") or die DBI::errstr;
+  my $dbh = DBI->connect("dbi:SQLite:dbname=./db.sqlite3") or die DBI::errstr;
   return $dbh;
   
 }
@@ -18,12 +19,28 @@ sub connect_db {
 set serializer => 'JSON';
 
 get '/data/:testid' => sub {
-  my $testid = param('testid');
+
+#  my $dbh = connect_db();
   
-  { testid => $testid,
-    time   => 'zero',
-    list   => [qw(1 2 3 4)],
+#  my $testid = param('testid');
+
+#  my $sth = $dbh->prepare('select jour, duree from historique where fk_id = ? ORDER BY jour ASC') or die $dbh->errstr;
+#  $sth->execute($testid) or die $sth->errstr;
+#  my $data = $sth->fetchall_hashref('jour');
+  
+  
+#  return $data;
+  
+  my $unixepoch = time ;
+
+  header('Access-Control-Allow-Origin', '*');
+  { jour  => $unixepoch,
+    duree => 1,
   }
+  
+  #  time   => 'zero',
+  #  list   => [qw(1 2 3 4)],
+  #}
 };
 
 
